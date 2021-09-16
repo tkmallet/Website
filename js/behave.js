1166b613d2a26343244d6c2f221b5ffdcc07598c
Theo Mallet <55764237+tkmallet@users.noreply.github.com>
2021-09-16 07:40:47 -0500

    commit

--- js/behave.js
+++ js/behave.js
@@ -1,5 +1,5 @@
 function navbar_responsive() {
-    var x = document.getElementById("navbarMobile");
+    var x = document.getElementById("myNavbar");
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
