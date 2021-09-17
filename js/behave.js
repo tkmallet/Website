1a3fb34baef344c99219cd1d75009da6ac554382
Theo Mallet <55764237+tkmallet@users.noreply.github.com>
2021-09-17 10:53:44 -0500

    finally corrected all nav behaviors

--- js/behave.js
+++ js/behave.js
@@ -1,22 +1,25 @@
 const menuBars = document.querySelector('.menuBars');
 let menuOpen = false;
+
 menuBars.addEventListener('click', () => {
   if (!menuOpen) {
+    openNav();
     menuBars.classList.add('open');
     menuOpen = true;
   }
   else {
+    closeNav();
     menuBars.classList.remove('open');
     menuOpen = false;
   }
-  navbarExpand();
 })
 
-function navbarExpand() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
+function openNav() {
+  document.getElementById("navbarID").style.height = "150px";
+  document.getElementById("mainBody").style.marginTop = "0px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav() {
+  document.getElementById("navbarID").style.height = "50px";
+  document.getElementById("mainBody").style.marginTop = "0";
+}
